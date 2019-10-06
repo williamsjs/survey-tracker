cfb1a09615de7812fc7015f8139148201b41c180
williamsjs <williamsjs1224@gmail.com>
2019-10-05 23:46:37 -0400

    Remove public dir from git

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
 </head>
 <body>
     <div id="app">
+        <root-component></root-component>
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
